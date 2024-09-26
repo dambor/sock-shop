78c8a6993f20fae8c2daedfd66e8bb2198c52438
dambor <glenioborges@gmail.com>
2024-09-26 14:41:14 -0400

    rollout start

--- spot-webhook.sh
+++ spot-webhook.sh
@@ -2,4 +2,4 @@ helm repo add castai-helm https://castai.github.io/helm-charts
 helm upgrade -i --create-namespace -n castai-pod-node-lifecycle castai-pod-node-lifecycle \
     castai-helm/castai-pod-node-lifecycle \
     --set staticConfig.preset=allSpotExceptKubeSystem \
-    --set staticConfig.defaultToSpot=false --set staticConfig.spotPercentageOfReplicaSet=20     
+    --set staticConfig.defaultToSpot=false --set staticConfig.spotPercentageOfReplicaSet=80     
